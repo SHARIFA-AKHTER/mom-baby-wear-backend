15ec5692494ae047e1bb6015d03333f15f531da3
unknown <sr0589071@gmail.com>
2025-12-09 17:05:59 +0600

    added an DashboardController

--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -2,19 +2,23 @@
 import { catchAsync } from "../../utils/catchAsync";
 import { DashboardService } from "./dashboard.service";
 
-export const DashboardController = {
-  getStats: catchAsync(async (req: any, res: { json: (arg0: { success: boolean; data: { totalUsers: any; totalOrders: any; totalRevenue: number; totalProducts: number; }; }) => void; }) => {
+
+ const getStats = catchAsync(async (req: any, res: { json: (arg0: { success: boolean; data: { totalUsers: any; totalOrders: any; totalRevenue: number; totalProducts: number; }; }) => void; }) => {
     const data = await DashboardService.getStats();
     res.json({ success: true, data });
-  }),
+  })
 
-  getMonthlySales: catchAsync(async (req: any, res: { json: (arg0: { success: boolean; data: unknown; }) => void; }) => {
+ const getMonthlySales = catchAsync(async (req: any, res: { json: (arg0: { success: boolean; data: unknown; }) => void; }) => {
     const data = await DashboardService.getMonthlySales();
     res.json({ success: true, data });
-  }),
+  })
 
-  getLowStock: catchAsync(async (req: any, res: { json: (arg0: { success: boolean; data: any; }) => void; }) => {
+  const getLowStock = catchAsync(async (req: any, res: { json: (arg0: { success: boolean; data: any; }) => void; }) => {
     const data = await DashboardService.getLowStockProducts();
     res.json({ success: true, data });
-  }),
+  })
+  export const DashboardController = {
+getStats,
+getMonthlySales,
+getLowStock
 };
