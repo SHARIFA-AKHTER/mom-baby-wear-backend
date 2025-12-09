c71e4bb477a16285a082ed83088749727dcdd4aa
unknown <sr0589071@gmail.com>
2025-12-09 17:04:06 +0600

    added an admin

--- src/modules/adminSettings/adminSettings.controller.ts
+++ src/modules/adminSettings/adminSettings.controller.ts
@@ -2,20 +2,24 @@
 import { catchAsync } from "../../utils/catchAsync";
 import { AdminSettingsService } from "./adminSettings.service";
 
-export const AdminSettingsController = {
-  setSetting: catchAsync(async (req: { body: { key: any; value: any; }; }, res: { json: (arg0: { success: boolean; message: string; data: any; }) => void; }) => {
+
+ const setSetting = catchAsync(async (req: { body: { key: any; value: any; }; }, res: { json: (arg0: { success: boolean; message: string; data: any; }) => void; }) => {
     const { key, value } = req.body;
     const result = await AdminSettingsService.setSetting(key, value);
     res.json({ success: true, message: "Setting updated", data: result });
-  }),
+  })
 
-  getSettings: catchAsync(async (req: any, res: { json: (arg0: { success: boolean; data: any; }) => void; }) => {
+  const getSettings = catchAsync(async (req: any, res: { json: (arg0: { success: boolean; data: any; }) => void; }) => {
     const result = await AdminSettingsService.getSettings();
     res.json({ success: true, data: result });
-  }),
+  })
 
-  getSetting: catchAsync(async (req: { params: { key: string; }; }, res: { json: (arg0: { success: boolean; data: any; }) => void; }) => {
+  const getSetting = catchAsync(async (req: { params: { key: string; }; }, res: { json: (arg0: { success: boolean; data: any; }) => void; }) => {
     const result = await AdminSettingsService.getSetting(req.params.key);
     res.json({ success: true, data: result });
-  }),
+  })
+  export const AdminSettingsController = {
+setSetting,
+getSettings,
+getSetting
 };
--- src/modules/adminSettings/adminSettings.service.ts
+++ src/modules/adminSettings/adminSettings.service.ts
@@ -1,22 +1,27 @@
 import { prisma } from "../../app/shared/prisma";
+import { ApiError } from "../../utils/ApiError";
 
 
-export const AdminSettingsService = {
-  async setSetting(key: string, value: string) {
+
+  const setSetting = (key: string, value: string) =>{
     return prisma.adminSettings.upsert({
       where: { key },
       update: { value },
       create: { key, value },
     });
-  },
+  }
 
-  async getSettings() {
+ const getSettings = () =>{
     return prisma.adminSettings.findMany();
-  },
+  }
 
-  async getSetting(key: string) {
+const getSetting = async (key: string) =>{
     const setting = await prisma.adminSettings.findUnique({ where: { key } });
     if (!setting) throw new ApiError(404, "Setting not found");
     return setting;
-  },
+  }
+  export const AdminSettingsService = {
+setSetting,
+getSettings,
+getSetting
 };
