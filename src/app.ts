9a2379d05c13bb7d43c46d20691c99844abc25f3
unknown <sr0589071@gmail.com>
2026-01-05 17:26:38 +0600

    origin, callback

--- src/app.ts
+++ src/app.ts
@@ -13,17 +13,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// app.use(
+//   cors({
+//     // origin: "http://localhost:3000",
+//     origin: [
+//       "http://localhost:3000", 
+//     "https://mom-baby-wear-frontend.vercel.app", 
+//     "mom-baby-wear-frontend-gdp93tc4c-sharifa991s-projects.vercel.app" 
+//       ],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+//   })
+// );
+
 app.use(
   cors({
-    // origin: "http://localhost:3000",
-    origin: [
-      "http://localhost:3000", 
-    "https://mom-baby-wear-frontend.vercel.app", 
-    "mom-baby-wear-frontend-gdp93tc4c-sharifa991s-projects.vercel.app" 
-      ],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://mom-baby-wear-frontend.vercel.app"
+      ];
+      
+     
+      if (!origin || allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 
