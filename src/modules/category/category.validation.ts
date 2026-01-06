5270079f7a60224b883b927e54a57234c42b38b7
unknown <sr0589071@gmail.com>
2026-01-06 20:23:10 +0600

    added an categoryValidation

--- src/modules/category/category.validation.ts
+++ src/modules/category/category.validation.ts
@@ -5,7 +5,7 @@ export const categoryValidation = z.object({
 body: z.object({
 name: z.string().min(1),
 description: z.string().optional(),
-image: z.string().optional()
+image:z.string().min(1, "Category image is required")
 }),
   query: z.object({}).optional(),
   params: z.object({}).optional(),
